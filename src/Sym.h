40130c14fa33e033cf749b428f21fcee3801a34d
Lech Szymanski <lechszym@cs.otago.ac.nz>
2015-03-04 16:47:29 +1300

    Implementing Sym class

--- src/Sym.h
+++ src/Sym.h
@@ -11,22 +11,25 @@
 #include <string>
 
 namespace gca {
- 
+  
    class Sym {
    public:
 
       Sym() {
-         _sign = 1;
+         _v = 0.0;
+         _inv = false;
       }
 
       Sym(const char s[]) {
-         _sign = 1;
+         _v = 1.0;
          _sym = std::string(s);
+         _inv = false;
       }
       
       Sym(const Sym& orig) {
-         _sign = orig._sign;
+         _v = orig._v;
          _sym = orig._sym;
+         _inv = orig._inv;
       }
 
       virtual ~Sym() {
@@ -47,19 +50,41 @@ namespace gca {
 
       Sym operator*(const int x) const {
          Sym m = Sym(*this);
-         m._sign *= x;
+         m._v *= x;
          return m;
+      }
+
+      int getSign() const {
+          int sign = 1;
+          if(_v < 0) {
+              sign *= -1;
+          }
+          for(std::size_t i=0;i<_mult.size();i++) {
+              sign *= _mult[i].getSign();
+          }
+          return sign;
+      }
+      
+      std::string toString() const {
+        std::stringstream ss;
+
+        ss << _sym;
+        
+        return ss.str();
+        
       }
       
       friend std::ostream& operator<<(std::ostream &out, const Sym &s) {      
-         out << s._sym;
+         out << s.toString();
          return out;      
       }
    
    private:
       std::string _sym;
-      int         _sign;
-      
+      double      _v;
+      bool        _inv;
+      std::vector<Sym> _mult;
+      std::vector<Sym> _sum;
    };
    
 }
